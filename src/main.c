dd1a1a466bebc318b5ef97c01801f449d8f107b2
rexept <io@etriadi.com>
2025-10-23 20:40:10 -0700

    feat: add locale check

--- src/main.c
+++ src/main.c
@@ -1,5 +1,8 @@
+#include <locale.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <X11/Xlib.h>
 
 #include "util.h"
 int main(int argc, char *argv[]) {
@@ -7,6 +10,9 @@ int main(int argc, char *argv[]) {
     die("aero"VERSION);
   else if (argc != 1)
     die("usage: aero [-v]");
+
+  if (!setlocale(LC_CTYPE, "") || !XSupportsLocale())
+      fputs("warning: no locale support\n", stderr);
 
   return EXIT_SUCCESS;
 }
