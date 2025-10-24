c2dab3c726101f4986d46c67dc2293ed7ca05e4f
rexept <io@etriadi.com>
2025-10-23 20:43:22 -0700

    fix(cli): fix version output format

--- src/main.c
+++ src/main.c
@@ -7,7 +7,7 @@
 #include "util.h"
 int main(int argc, char *argv[]) {
   if (argc == 2 && !strcmp("-v", argv[1]))
-    die("aero"VERSION);
+    die("aero-"VERSION);
   else if (argc != 1)
     die("usage: aero [-v]");
 
