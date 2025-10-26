89eaecfa16f8beea85c3c6ab1b83e2e4005f29bd
rexept <io@etriadi.com>
2025-10-26 15:34:21 -0700

    feat: add ISVISIBLE macro

--- src/util.h
+++ src/util.h
@@ -5,6 +5,7 @@
 #define MAX(A, B)               ((A) > (B) ? (A) : (B))
 #define MIN(A, B)               ((A) < (B) ? (A) : (B))
 #define BETWEEN(X, A, B)        ((A) <= (X) && (X) <= (B))
+#define ISVISIBLE(C)            ((C->tags & C->mon->tagset[C->mon->seltags]))
 #define LENGTH(X)               (sizeof X / sizeof X[0])
 // clang-format on
 
