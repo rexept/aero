d34b36855d67fa5339a594f487634f7b937e5b74
rexept <io@etriadi.com>
2025-10-26 01:49:45 -0700

    feat(util): add useful macros

--- src/util.h
+++ src/util.h
@@ -1,6 +1,13 @@
 #ifndef UTIL_H
 #define UTIL_H
 
+// clang-format off
+#define MAX(A, B)               ((A) > (B) ? (A) : (B))
+#define MIN(A, B)               ((A) < (B) ? (A) : (B))
+#define BETWEEN(X, A, B)        ((A) <= (X) && (X) <= (B))
+#define LENGTH(X)               (sizeof X / sizeof X[0])
+// clang-format on
+
 #include <X11/Xlib.h>
 
 void die(const char *fmt, ...);
